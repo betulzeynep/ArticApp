acaf7cec3d0fc0e963466ef2574ba6be16656f9b
LogicAndLines <betulzeynepseyis@gmail.com>
2025-09-28 20:52:21 +0200

    Initial Commit

---
+++ ArticApp/ArticAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticAppApp.swift
+//  ArticApp
+//
+//  Created by Zeynep Seyis on 28.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ArticAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
