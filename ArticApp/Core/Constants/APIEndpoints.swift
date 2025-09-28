9c1afb6c39ad18817cd4f1d8dc19f8431252564e
LogicAndLines <betulzeynepseyis@gmail.com>
2025-09-28 21:22:20 +0200

    API Endpoints for general usage added
    baseURL & artwork search endpoint were added for data retrieval
    IIIFBaseURL was added for image URL generation

---
+++ ArticApp/Core/Constants/APIEndpoints.swift
@@ -0,0 +1,14 @@
+//
+//  APIEndpoints.swift
+//  ArticApp
+//
+//  Created by Zeynep Seyis on 28.09.2025.
+//
+
+import Foundation
+
+struct APIEndpoints {
+    static let baseURL = "https://api.artic.edu/api/v1"
+    static let artworksSearch = "/artworks/search"
+    static let iiifBaseURL = "https://www.artic.edu/iiif/2"
+}
